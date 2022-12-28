ed845439500c6cd27154219757d8271aa82bd9a1
Marcos <marcos@pop-os.localdomain>
2022-12-28 20:39:24 -0300

    Resolvido problema da leitura do livro

--- 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/desafios-js-029/04-leituraLivro.js
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/desafios-js-029/04-leituraLivro.js
@@ -1,3 +1,7 @@
 /* Ana começou a ler o livro "O guia do mochileiro das galáxias", que tem 208 páginas. Após 2 minutos ela leu 4 páginas. 
 Crie um algoritmo que calcula e imprime o tempo em minutos que Ana levará para ler todo o livro seguindo o formato:
 Ana levará X minutos para ler o livro todo. */
+let totalDePaginas = 208;
+let paginasPorMinuto = 2;
+let tempoDaLeitura = totalDePaginas / paginasPorMinuto;
+console.log('Ana levará ' + tempoDaLeitura + ' minutos para ler o livro todo.');
