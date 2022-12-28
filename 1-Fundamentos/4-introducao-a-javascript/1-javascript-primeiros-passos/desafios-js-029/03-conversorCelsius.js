9c5ab9c73df0bb0d1b0a198b466869321b5a7660
Marcos <marcos@pop-os.localdomain>
2022-12-28 20:32:04 -0300

    Criaçao de algoritmo que faz a conversao de celsius para fahrenheit

--- 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/desafios-js-029/03-conversorCelsius.js
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/desafios-js-029/03-conversorCelsius.js
@@ -1,3 +1,7 @@
 /* Escreva um algoritmo que faça conversão de temperatura em graus Celsius para graus Fahrenheit.
 DICA: A conversão deve ser a temperatura em graus Celsius dividido por 5, o resultado multiplicado por 9 somado com 32.
 Exiba o resultado no console. */
+
+let Celsius = 20;
+let Fahrenheit = Celsius / 5 * 9 + 32
+console.log('Convertendo em Fahrenheit o resultado é ' + Fahrenheit + 'F.')
