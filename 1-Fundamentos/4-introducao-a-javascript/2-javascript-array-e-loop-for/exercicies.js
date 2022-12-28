2ff1fb9bc273f781600d1373442fd8b7198e78ba
Marcos <marcos@pop-os.localdomain>
2022-12-28 19:45:55 -0300

    Faz laço de repetição pra encontrar maior número do array

--- 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercicies.js
+++ 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercicies.js
@@ -1,15 +1,24 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-    result += numbers[index];
-}
-//console.log(result);
-let media = result / numbers.length;
-console.log(media);
+// let result = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     result += numbers[index];
+// }
+// //console.log(result);
+// let media = result / numbers.length;
+// console.log(media);
 
-if (media > 20){
-    console.log('Valor maior que 20');
+// if (media > 20){
+//     console.log('Valor maior que 20');
+// }
+// else {
+//     console.log('Valor menor ou igual a 20');
+// }
+
+let biggerNumber = numbers [0];
+
+for(let index =  1; index < numbers.length; index += 1){
+    if(numbers [index] > biggerNumber){
+        biggerNumber = numbers [index];
+    }
 }
-else {
-    console.log('Valor menor ou igual a 20');
-}
+console.log(biggerNumber);
